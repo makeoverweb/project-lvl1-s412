577e003b8af7e51fbcc32059df8b22c092929fa7
makeoverWeb <femakeoverweb@gmail.com>
2019-01-14 22:58:32 +0300

    add greetingUser

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from '/readline-sync';
+
+export const actual = () => {
+    const name = readlineSync.question('May I have your name? ');
+    console.log('Hellow ' + name + '!');
+}
