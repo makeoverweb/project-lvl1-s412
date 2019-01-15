f0d9978cbc09105b7666b88b669f26658a2a94c9
makeoverWeb <femakeoverweb@gmail.com>
2019-01-15 17:43:43 +0300

    fix bag rename const

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const actual = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hellow ' + name + '!');
+export const greetingUser = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
 }
