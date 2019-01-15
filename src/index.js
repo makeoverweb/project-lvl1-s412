4946253334a4ce5fb53df73ab9cc420c717e06b2
makeoverWeb <femakeoverweb@gmail.com>
2019-01-15 19:28:38 +0300

    fixed bag step2

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 export const greetingUser = () => {
     const userName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${userName}!`);
-}
+};
