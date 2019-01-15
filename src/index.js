ca634899c993c97b677839cc21a5ce8b27c673a1
makeoverWeb <femakeoverweb@gmail.com>
2019-01-15 21:29:24 +0300

    fixed bag eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export const greetingUser = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+const greetingUser = () => {
+  console.log('Welcome to the Brain Games!\n');
+
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
+
+
+export default greetingUser;
